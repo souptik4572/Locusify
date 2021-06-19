836ee0dd63941fdab57b84e0428f95aeb9bde4d2
souptik4572 <souptiksarkar4572@gmail.com>
2021-06-20 00:20:56 +0530

    final submission update

--- public/script.js
+++ public/script.js
@@ -1,29 +1,65 @@
+const arrayToObject = (arr) => arr.reduce((acc, curr) => ((acc[curr] = null), acc), {});
+
+const App = function _App() {};
+
+App.state = {
+	names: {},
+	locations: {},
+	setNames: (newNames) => {
+		App.state.names = newNames;
+	},
+	setLocations: (newLocations) => {
+		App.state.locations = newLocations;
+	},
+};
+
 document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems, {});
+	var instances = M.Sidenav.init(elems, {
+		data: App.state.names,
+	});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.tooltipped');
-	var instances = M.Tooltip.init(elems, {});
+	var instances = M.Tooltip.init(elems, {
+		data: App.state.locations,
+	});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-	var elems = document.querySelectorAll('.autocomplete');
-	var instances = M.Autocomplete.init(elems, {});
+	var elems = document.querySelectorAll('#place_name');
+	var instances = M.Autocomplete.init(elems, {
+		data: App.state.names,
+	});
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-	var elems = document.querySelectorAll('.autocomplete');
-	var instances = M.Autocomplete.init(elems, {});
-});
+// const initializeAutocomplete = (names = {}, locations = {}) => {
+// 	console.log(names, locations);
+// 	document.addEventListener('DOMContentLoaded', function () {
+// 		var elems = document.querySelectorAll('#place_name');
+// 		var instances = M.Autocomplete.init(elems, {
+// 			data: names,
+// 		});
+// 	});
+
+// 	document.addEventListener('DOMContentLoaded', function () {
+// 		var elems = document.querySelectorAll('#place_location');
+// 		var instances = M.Autocomplete.init(elems, {
+// 			data: locations,
+// 		});
+// 	});
+// };
 
 let uniqueData = {};
 const fetchUniqueAttributeData = async () => {
 	axios
-		.get('localhost:3000/places/unique')
+		.get('http://localhost:3000/places/unique')
 		.then((response) => {
-			console.log(response);
+			const { names, locations } = response.data;
+			App.state.setNames(arrayToObject(names));
+			App.state.setLocations(locations);
+			// initializeAutocomplete(arrayToObject(names), arrayToObject(locations));
 		})
 		.catch((error) => {
 			console.log('Oops got an error while fetching unique data', error);
