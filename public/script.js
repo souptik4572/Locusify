61194d8927b26fbc76dc565a9ab57a0b1b273e2d
souptik4572 <souptiksarkar4572@gmail.com>
2021-06-20 10:52:47 +0530

    completed auto complete and added pacifico font

--- public/script.js
+++ public/script.js
@@ -1,55 +1,25 @@
 const arrayToObject = (arr) => arr.reduce((acc, curr) => ((acc[curr] = null), acc), {});
 
-const App = function _App() {};
-
-App.state = {
-	names: {},
-	locations: {},
-	setNames: (newNames) => {
-		App.state.names = newNames;
-	},
-	setLocations: (newLocations) => {
-		App.state.locations = newLocations;
-	},
-};
-
 document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems, {
-		data: App.state.names,
-	});
+	var instances = M.Sidenav.init(elems, {});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.tooltipped');
-	var instances = M.Tooltip.init(elems, {
-		data: App.state.locations,
-	});
+	var instances = M.Tooltip.init(elems, {});
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-	var elems = document.querySelectorAll('#place_name');
-	var instances = M.Autocomplete.init(elems, {
-		data: App.state.names,
+const initializeAutocomplete = (names = {}, locations = {}) => {
+	const elemNames = document.querySelector('#place_name');
+	const nameInstances = M.Autocomplete.init(elemNames, {
+		data: names,
 	});
-});
-
-// const initializeAutocomplete = (names = {}, locations = {}) => {
-// 	console.log(names, locations);
-// 	document.addEventListener('DOMContentLoaded', function () {
-// 		var elems = document.querySelectorAll('#place_name');
-// 		var instances = M.Autocomplete.init(elems, {
-// 			data: names,
-// 		});
-// 	});
-
-// 	document.addEventListener('DOMContentLoaded', function () {
-// 		var elems = document.querySelectorAll('#place_location');
-// 		var instances = M.Autocomplete.init(elems, {
-// 			data: locations,
-// 		});
-// 	});
-// };
+	const elemLocations = document.querySelector('#place_location');
+	const locationInstances = M.Autocomplete.init(elemLocations, {
+		data: locations,
+	});
+};
 
 let uniqueData = {};
 const fetchUniqueAttributeData = async () => {
@@ -57,9 +27,7 @@ const fetchUniqueAttributeData = async () => {
 		.get('http://localhost:3000/places/unique')
 		.then((response) => {
 			const { names, locations } = response.data;
-			App.state.setNames(arrayToObject(names));
-			App.state.setLocations(locations);
-			// initializeAutocomplete(arrayToObject(names), arrayToObject(locations));
+			initializeAutocomplete(arrayToObject(names), arrayToObject(locations));
 		})
 		.catch((error) => {
 			console.log('Oops got an error while fetching unique data', error);
